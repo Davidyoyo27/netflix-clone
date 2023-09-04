c7dfbd982f9df7c2e512caa0da960f8791f3c130
Davidyoyo27 <davidca.vega@gmail.com>
2023-09-03 23:13:30 -0300

    modulo Mi lista

--- src/components/MiLista.vue
+++ src/components/MiLista.vue
@@ -1,13 +1,28 @@
 <template>
-  <h1>aca va mi lista</h1>
+  <div class="wrapper">
+    <div class="container">
+      <h1>Mi lista</h1>
+      <span>Aca iría "Mi lista" en caso de que existiera</span>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-
+<style scoped>
+.wrapper {
+  position: relative;
+  z-index: 1;
 }
-</script>
 
-<style>
+.container {
+  margin: 5rem 0rem 0rem 4rem;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  color: #fff;
+}
 
+h1{
+  font-size: 2.3rem;
+  font-weight: 500;
+}
 </style>
